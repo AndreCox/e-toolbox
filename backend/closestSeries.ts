46bc8f03ce529efe69d40aae6f5726a3157a1244
Andre Cox <arc.xoc@gmail.com>
2023-02-07 22:38:57 -0800

    Other Stuff

---
+++ backend/closestSeries.ts
@@ -0,0 +1,18 @@
+import eseries from "./eseries";
+
+export default function closestSeries(series: number, value: number) {
+  let E = eseries(series);
+  E.push(E[0] * 10);
+
+  let expon = Math.floor(Math.log10(value));
+  let Ei = value / Math.pow(10, expon);
+
+  let err = E.map((e) => Math.abs(e - Ei) / e);
+  let min_m = Math.min(...err);
+  let m = err.indexOf(min_m);
+
+  let out = E[m] * Math.pow(10, expon);
+  let out_err = (out - value) / value;
+
+  return { out, out_err };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,76 +2,95 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { Input, Card, Select, Option } from "@material-tailwind/react";
+import closestSeries from "../backend/closestSeries";
+import { useEffect, useState } from "react";
+import githubLogo from "../assets/images/github-mark.svg";
 
 export default function Home() {
+  const [Eseries, setEseries] = useState(3);
+  const [ArbitraryValue, setArbitraryValue] = useState(0);
+  const [StandardValue, setStandardValue] = useState(0);
+  const [Error, setError] = useState(0);
+
+  useEffect(() => {
+    let output = closestSeries(Eseries, ArbitraryValue);
+    setStandardValue(output.out);
+    setError(output.out_err);
+  }, [Eseries, ArbitraryValue]);
+
   return (
-    <div className="p-[0 2rem]">
+    <div className="">
       <Head>
         <title>Next Template</title>
         <meta name="description" content="Generated with next-template" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen p-4 flex flex-1 flex-col justify-center items-center text-4xl">
-        <h1 className="font-bold text-center">
-          Welcome to{" "}
-          <a
-            href="https://github.com/andrecox/next-template"
-            className="text-blue-500 underline decoration-transparent hover:decoration-blue-500 transition-all"
-          >
-            Next Template!
-          </a>
-        </h1>
-        <h2 className="mt-2">
-          Powered by{" "}
-          <a
-            href="https://nextjs.org"
-            className="text-blue-500 underline decoration-transparent hover:decoration-blue-500 transition-all"
-          >
-            Next.js
-          </a>
-        </h2>
+      <a href="https://github.com" target="_blank" rel="noreferrer">
+        <Image
+          alt="Github Logo"
+          src={githubLogo}
+          width={70}
+          height={70}
+          className="fixed right-0 top-0 p-2"
+        />
+      </a>
 
-        <p className="text-center m-16 text-[1.5rem]">
-          Get started by editing{" "}
-          <code className="bg-[#fafafa] rounded-md p-[0.75rem] font-mono text-[1.1rem]">
-            pages/index.js
-          </code>
-        </p>
+      <main className="min-h-screen space-y-8 p-4 flex flex-1 flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold text-center">E Toolbox</h1>
 
-        <div className="flex items-center justify-center flex-wrap max-w-[800px]">
-          <a href="https://tailwindcss.com" className={styles.card}>
-            <h2>Tailwind CSS &rarr;</h2>
-            <p>
-              Learn about Tailwind keeps the app size small while still being
-              easy to use.
-            </p>
-          </a>
-
-          <a href="https://mobx.js.org/README.html" className={styles.card}>
-            <h2>MobX &rarr;</h2>
-            <p>A powerful way to manage state without tons of boilerplate!</p>
-          </a>
-
-          <a href="https://pages.github.com/" className={styles.card}>
-            <h2>Github Pages &rarr;</h2>
-            <p>Build a static site and deploy it using Github Actions.</p>
-          </a>
-
-          <a href="https://capacitorjs.com" className={styles.card}>
-            <h2>Capacitor JS &rarr;</h2>
-            <p>Build your web app for mobile with access to native features.</p>
-          </a>
-        </div>
-        <p className="text-center mt-16 text-[1.5rem]">
-          MobX state management built in!
-        </p>
-        <Link href="/mobx">
-          <div className="text-blue-500 under text-xl underline decoration-transparent  hover:decoration-blue-500 transition-all">
-            Check it out here
+        <Card className="p-2">
+          <h2 className="text-center text-2xl p-2 mb-4">E Series Calculator</h2>
+          <div className="p-2 flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+            <Select
+              label="E Series Value"
+              onChange={(e) => {
+                setEseries(Number(e));
+              }}
+            >
+              <Option value="3">3</Option>
+              <Option value="6">6</Option>
+              <Option value="12">12</Option>
+              <Option value="24">24</Option>
+              <Option value="48">48</Option>
+              <Option value="96">96</Option>
+              <Option value="192">192</Option>
+            </Select>
+            <Input
+              label="Arbitrary Value"
+              type="number"
+              onChange={(e) => {
+                setArbitraryValue(Number(e.target.value));
+              }}
+            />
           </div>
-        </Link>
+          <div className="p-4 flex sm:flex-row flex-col text-center justify-around">
+            <div>
+              <h3>Standard Value</h3>
+              <h3 className="text-blue-500 font-bold text-center text-lg">
+                {isNaN(StandardValue) ? "0" : StandardValue}
+              </h3>
+            </div>
+            <div>
+              <h3>Standard Error</h3>
+              <h3 className="text-blue-500 font-bold text-center text-lg">
+                {isNaN(Error) ? "0" : (Error * 100).toFixed(4)}%
+              </h3>
+            </div>
+          </div>
+        </Card>
       </main>
+      <div className="fixed bottom-0 left-0 w-full p-2 ">
+        <p>
+          Original Octave code by{" "}
+          <span className="text-blue-500 font-bold">Michael Bell</span>
+        </p>
+        <p>
+          GUI and Typescript port by{" "}
+          <span className="text-blue-500 font-bold">Andre Cox</span>
+        </p>
+      </div>
     </div>
   );
 }
